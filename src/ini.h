40bdb6a41e036778ffa33fe28214031a63f53f3d
rxi <rxi@users.noreply.github.com>
2015-09-17 21:10:06 +0100

    Added version to header, reformatted

--- src/ini.h
+++ src/ini.h
@@ -8,15 +8,13 @@
 #ifndef INI_H
 #define INI_H
 
+#define INI_VERSION "0.1.0"
+
 typedef struct ini_t ini_t;
 
-
 ini_t*      ini_load(const char *filename);
-
 void        ini_free(ini_t *ini);
-
 const char* ini_get(ini_t *ini, const char *section, const char *key);
-
 int         ini_sget(ini_t *ini, const char *section, const char *value,
                      const char *scanfmt, void *dst);
 
